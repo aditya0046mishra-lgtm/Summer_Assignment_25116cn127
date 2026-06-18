33c05106c3d2ec2b4f1d7ecb9e5832739f3b1cc3
Aditya Mishra <adityamishra@Adityas-MacBook-Air-2.local>
2026-06-18 06:18:31 +0530

    Added Day 19 cpp files

---
+++ Day_19/Ques75.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int r,c;
+    cout<<"Enter rows and columns :- ";
+    cin>>r>>c;
+    int arr[r][c];
+    cout<<"Enter matrix :- "<<endl;
+    for(int i=0;i<r;i++) {
+        for(int j=0;j<c;j++) {
+            cin>>arr[i][j];
+        }
+    }
+    cout<<"Transpose Matrix :- "<<endl;
+    for(int i=0;i<c;i++) {
+        for(int j=0;j<r;j++) {
+            cout<<arr[j][i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
---
+++ Day_19/Ques76.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    int sum=0;
+    cout<<"Enter size of square matrix :- ";
+    cin>>n;
+    int arr[n][n];
+    cout<<"Enter matrix :- "<<endl;
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<n;j++) {
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<n;i++) {
+        sum += arr[i][i];
+    }
+    cout<<"Diagonal Sum = "<<sum;
+    return 0;
+}
