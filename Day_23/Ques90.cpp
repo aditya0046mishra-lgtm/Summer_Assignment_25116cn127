0cfe7c2d0b5145f4811dd53ad565d214c3f834f7
Aditya Mishra <adityamishra@Adityas-MacBook-Air-2.local>
2026-06-19 09:17:22 +0530

    Added Day 23 cpp files

---
+++ Day_23/Ques90.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char str[100];
+    cout<<"Enter string :- ";
+    cin.getline(str,100);
+    for(int i=0;str[i]!='\0';i++){
+        for(int j=i+1;str[j]!='\0';j++){
+            if(str[i]==str[j]){
+                cout<<"First Repeating Chsracter = "<<str[i];
+            }
+        }
+    }
+    cout<<"No repeating character";
+    return 0;
+}
